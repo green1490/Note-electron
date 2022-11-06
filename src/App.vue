e0664f0691594ae5cb005d365b15380a21fec8b5
green1490 <zambokrisztian1490@gmail.com>
2022-11-06 21:55:50 +0100

    add sidebar

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,23 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import Sidebar from './components/Sidebar.vue'
 </script>
 
 <template>
-  <div class="logo-box">
-    <img class="logo vite" src="./assets/vite.svg" >
-    <img class="logo electron" src="./assets/electron.svg" >
-    <img class="logo vue" src="./assets/vue.svg" >
-  </div>
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
-  <div class="static-public">
-    Place static files into the <code>/public</code> folder
-    <img style="width:77px;" :src="'./node.png'" >
-  </div>
+  <!-- <div id="#app">
+    <div class="grid-container">
+      <div>1</div>
+      <div>2</div>
+      <div>3</div>  
+      <div>4</div>
+      <div>5</div>
+      <div>6</div>  
+      <div>7</div>
+      <div>8</div>
+    </div>
+  </div> -->
+  <Sidebar></Sidebar>
 </template>
 
 <style>
@@ -22,47 +25,22 @@ import HelloWorld from './components/HelloWorld.vue'
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  display: grid;
   color: #2c3e50;
-  margin-top: 60px;
 }
 
-.logo-box {
-  display: flex;
-  width: 100%;
-  justify-content: center;
+.grid-container {
+  display: grid;
+  grid-template-columns: 80px 200px auto 30px;
+  gap: 10px;
+  background-color: #2196F3;
+  padding: 10px;
 }
 
-.static-public {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-}
-
-.static-public code {
-  background-color: #eee;
-  padding: 2px 4px;
-  margin: 0 4px;
-  border-radius: 4px;
-  color: #304455;
-}
-
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: 0.75s;
-}
-
-.logo.vite:hover {
-  filter: drop-shadow(0 0 2em #747bff);
-}
-
-.logo.electron:hover {
-  filter: drop-shadow(0 0 2em #9FEAF9);
-}
-
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #249b73);
+.grid-container > div {
+  background-color: rgba(255, 255, 255, 0.8);
+  text-align: center;
+  padding: 20px 0;
+  font-size: 30px;
 }
 </style>
---
+++ src/components/Sidebar.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="sidenav">
+		<div class="icon">
+			<button  class="btn btn-outline-dark">
+				<i class="fa fa-solid fa-gear"></i>
+			</button>
+		</div>
+	</div>
+</template>
+
+<style scoped>
+.sidenav {
+  height: 100%;
+  width: 50px;
+  position: fixed;
+  z-index: 1;
+  top: 0;
+  left: 0;
+  background-color: #111;
+  overflow-x: hidden;
+  padding-top: 20px;
+}
+.icon,i {
+	text-decoration: none;
+	text-align: center;
+  font-size: 25px;
+  display: inline-block;
+}
+/*
+.sidenav button {
+  padding: 6px 16px 6px 20%;
+  text-decoration: none;
+	text-align: center;
+  font-size: 25px;
+  color: #818181;
+  display: inline-block;
+}
+*/
+.fa {
+	color: white;
+	}
+</style>
