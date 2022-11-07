fe57785f85191c561377a0b5cbc1b20d9be9c6fd
green1490 <zambokrisztian1490@gmail.com>
2022-11-07 23:06:05 +0100

    add fontawsome subset to be able to load into the sidebar and for optimisation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
-import { createApp } from 'vue'
+import { createApp, VueElement } from 'vue'
 import App from './App.vue'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faArrowRightArrowLeft,faFolder,faGear } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 // import './samples/node-api'
 
+library.add(faArrowRightArrowLeft,faFolder,faGear)
+
+
 createApp(App)
+  .component('font-awesome-icon',FontAwesomeIcon)
   .mount('#app')
   .$nextTick(() => {
     postMessage({ payload: 'removeLoading' }, '*')
